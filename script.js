3d03c8200cef44377439d3eca74f906ada8fcce3
saadsultan482 <saadsultan482@gmail.com>
2024-03-14 15:31:00 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,52 @@
+const notesContainer = document.querySelector(".notes-container");
+const createBtn = document.querySelector(".btn");
+let notes = document.querySelectorAll(".input-box");
+
+
+function showNotes() {
+    notesContainer.innerHTML = localStorage.getItem("notes");
+}
+showNotes();
+
+
+
+function updateStorage() {
+    localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
+
+
+
+createBtn.addEventListener("click", () => {
+    let inputbox = document.createElement("p");
+    let img = document.createElement("img");
+    inputbox.className = "input-box";
+    inputbox.setAttribute("contenteditable", "true");
+    img.src = "./images/delete.png";
+    notesContainer.appendChild(inputbox).appendChild(img);
+    inputbox.addEventListener('keyup', updateStorage);
+});
+
+
+notesContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "IMG") {
+        e.target.parentElement.remove();
+        updateStorage();
+    }
+    else if (e.target.tagName === "p") {
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach(nt => {
+            nt.onkeyup = function () {
+                updateStorage();
+            }
+        })
+    }
+})
+
+
+document.addEventListener("keydown", event => {
+    if (event.key === "Enter") {
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
+    }
+})
